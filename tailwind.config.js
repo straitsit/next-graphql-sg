37a561be47c09a1ff9cda689bb5c6ddea73b90b8
Andrew Fam <andrewfam@Peters-MacBook-Pro.local>
2021-06-07 14:16:32 +0800

    Clean up of config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,24 @@
+const colors = require('tailwindcss/colors');
+
 module.exports = {
-  darkMode: false,
-  plugins: [],
-  purge: [
-    './pages/**/*.{js,ts,jsx,tsx}',
-    './components/**/*.{js,ts,jsx,tsx}'
-  ],
+  purge: ['./src/pages/**/*.{js,ts,jsx,tsx}', './src/components/**/*.{js,ts,jsx,tsx}'],
+  darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {}
+    extend: {
+      colors: {
+        'light-blue': colors.lightBlue,
+        teal: colors.teal,
+        cyan: colors.cyan,
+        rose: colors.rose,
+      }
+    }
   },
   variants: {
-    extend: {}
-  }
+    extend: {},
+  },
+  plugins: [
+    // ...
+    require('@tailwindcss/forms'),
+    require('@tailwindcss/line-clamp'),
+  ]
 }
